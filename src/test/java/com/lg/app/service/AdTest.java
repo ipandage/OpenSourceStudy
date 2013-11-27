fd20011083aab78379e2d14ac6459ff22ab05199
pandage <pandage@pandage-PC>
2013-11-27 21:36:01 +0800

    修改测试类

--- src/test/java/com/lg/app/service/AdTest.java
+++ src/test/java/com/lg/app/service/AdTest.java
@@ -3,15 +3,19 @@ package com.lg.app.service;
 import java.util.Date;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lg.app.entity.Ad;
-
-@ContextConfiguration(locations = { "/applicationContext.xml" })
-@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=false)
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(locations = "classpath:applicationContext.xml")  
+@Transactional  
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)  
 public class AdTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
