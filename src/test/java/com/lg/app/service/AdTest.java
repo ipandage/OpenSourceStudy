4d31f13515e35b53e8cdd0fa71ce25bc782d9338
pandage <pandage@pandage-PC>
2013-11-27 21:24:25 +0800

    加入测试文件

--- src/test/java/com/lg/app/service/AdTest.java
+++ src/test/java/com/lg/app/service/AdTest.java
@@ -1,22 +1,29 @@
 package com.lg.app.service;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.lg.app.entity.Ad;
 
 @ContextConfiguration(locations = { "/applicationContext.xml" })
+@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=false)
 public class AdTest extends AbstractTransactionalJUnit4SpringContextTests {
 
-	@Autowired
-	private AdService adService;
+    @Autowired
+    private AdService adService;
 
 	@Test
 	public void list() {
-		System.out.println("1");
+		Ad ad = new Ad();
+		ad.setCreateTime(new Date());
+		ad.setDescription("hello");
+		ad.setName("gao");
+		adService.save(ad);
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("111");
-	}
 }
