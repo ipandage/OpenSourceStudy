392f26f65f8eec850390442bb67a3719278d7a83
pandage <pandage@pandage-PC>
2013-11-27 21:45:09 +0800

    修改测试文件

--- src/test/java/com/lg/app/service/AdTest.java
+++ src/test/java/com/lg/app/service/AdTest.java
@@ -1,6 +1,9 @@
 package com.lg.app.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,22 +15,49 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lg.app.entity.Ad;
-@RunWith(SpringJUnit4ClassRunner.class)  
-@ContextConfiguration(locations = "classpath:applicationContext.xml")  
-@Transactional  
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)  
+import com.lg.app.web.query.AdQueryCriterion;
+import com.lg.platform.dao.support.Page;
+import com.lg.platform.dao.support.Sort;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+@Transactional
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class AdTest extends AbstractTransactionalJUnit4SpringContextTests {
 
-    @Autowired
-    private AdService adService;
+	@Autowired
+	private AdService adService;
 
 	@Test
-	public void list() {
+	public void save() {
 		Ad ad = new Ad();
 		ad.setCreateTime(new Date());
 		ad.setDescription("hello");
 		ad.setName("gao");
 		adService.save(ad);
 	}
-	
+
+	@Test
+	public void list() {
+		List<Ad> list = adService.list("from Ad");
+
+		for (Ad ad : list) {
+			System.out.println(ad.getName());
+		}
+	}
+
+	@Test
+	public void listPage() {
+		Map<String, String> queryParams = new HashMap<String, String>();
+		Page<Ad> adPage = null;
+		if (adPage == null) {
+			adPage = new Page<Ad>();
+		}
+		if (adPage.getSort() == null) {
+			adPage.setSort(new Sort("id", Sort.DESC));
+		}
+		adPage = adService.listPage(new AdQueryCriterion(queryParams), adPage);
+		System.out.println(adPage.getTotalCount());
+	}
+
 }
