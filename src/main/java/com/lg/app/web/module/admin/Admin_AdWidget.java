674784a011009353d7e52aef989e2306b1cc21ac
Administrator <Administrator@95MJOCXJDELKL1Y>
2013-11-28 17:56:37 +0800

    修改Admin_AdWidget

--- src/main/java/com/lg/app/web/module/admin/Admin_AdWidget.java
+++ src/main/java/com/lg/app/web/module/admin/Admin_AdWidget.java
@@ -8,18 +8,19 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.lg.app.entity.Ad;
 import com.lg.app.service.AdService;
 import com.lg.app.support.log.Log;
 import com.lg.app.web.query.AdQueryCriterion;
 import com.lg.platform.dao.support.Page;
 import com.lg.platform.dao.support.Sort;
-import com.lg.platform.utils.SessionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Gao xingang
@@ -27,12 +28,13 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @description
  * @TODO
  */
+@Controller
 @RequestMapping("/admin/ad")
-public class Admin_AdWidget extends Admin_AbstractWidget {
-    @Autowired
-    private AdService adService;
+public class Admin_AdWidget {
+	
+	@Autowired
+	private AdService adService;
 
-    @SuppressWarnings("rawtypes")
     @RequestMapping("/list")
     @Log
     public String list(@ModelAttribute Page<Ad> adPage, @ModelAttribute Ad ad, @RequestParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
@@ -46,49 +48,4 @@ public class Admin_AdWidget extends Admin_AbstractWidget {
         model.addAttribute("adPage", adPage);
         return "/admin/ad/listAd";
     }
-
-    @RequestMapping("/create")
-    public String create(@ModelAttribute Ad ad, HttpServletRequest request, HttpServletResponse response, Model model) {
-        return "/admin/ad/createAd";
-    }
-
-    @RequestMapping("/create/save")
-    @Log
-    public String create_save(@ModelAttribute Ad ad, HttpServletRequest request, HttpServletResponse response, Model model) {
-        ad = adService.save(ad);
-        return "redirect:/admin/ad/list";
-    }
-
-    @RequestMapping("/edit")
-    public String edit(@RequestParam(required = false) Long id, @ModelAttribute Ad ad, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (id == null) {
-            id = SessionUtils.getAttrAsLong(request, "AdWidget_edit_id");
-        }
-        if (id == null) {
-            return prompt(model, "请先选择要修改的广告");
-        }
-
-        ad = adService.get(id);
-        if (ad == null) {
-            return prompt(model, "请先选择要修改的广告");
-        }
-        model.addAttribute("ad", ad);
-        SessionUtils.setAttr(request, "AdWidget_edit_id", id);
-        return "/admin/ad/editAd";
-    }
-
-    @RequestMapping("/edit/save")
-    @Log
-    public String edit_save(@ModelAttribute Ad ad, HttpServletRequest request, HttpServletResponse response, Model model) {
-        adService.update(ad);
-        return "redirect:/admin/ad/list";
-    }
-
-    @RequestMapping("/delete")
-    @Log
-    public String delete(@RequestParam(required = false) Long[] id, @ModelAttribute Page<Ad> adPage, @ModelAttribute Ad ad, @RequestParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        adService.remove(id);
-        return list(adPage, ad, queryParams, request, response, model);
-    }
-
 }
